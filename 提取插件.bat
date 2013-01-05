52d5927be5bce17b8cfe375e093a4f0938ae54ca
zhouhaipeng <zhouhaipeng@a948d3e7-c0ca-459f-a804-3cfd5e608bc6>
2013-01-05 14:26:24 +0000

    新增可获取完整文件夹作为系列插件
    git-svn-id: https://singingcicada.xicp.net/svn/seedplugins@75 a948d3e7-c0ca-459f-a804-3cfd5e608bc6

--- 提取插件.bat
+++ 提取插件.bat
@@ -1,29 +1,37 @@
 @echo off
 
 ::遍历目录,结果存至temp
+rd seed_plugins /S /Q
 dir /o:e >temp
 
 ::从遍历结果中找出文件夹,存入foldersList
+
 find "DIR" temp > foldersList
 del temp
 ::删除seed_plugins目录下所有文件
 
-rd seed_plugins /S /Q
+
 mkdir seed_plugins
-for /f "skip=5 tokens=3* " %%a in (foldersList) do (
+for /f "skip=4 tokens=3* " %%a in (foldersList) do (
 	cd %%b 
 	dir /o:e > plugins
-	::从文件夹中寻找.lua文件
 	find ".lua" plugins > fileList 
+	find "DIR" plugins > folder
 	del plugins
-	::拷贝相应文件至seed_plugins目录同名文件
 	for /f "skip=1 tokens=3* " %%c in (fileList) do (
 		copy %%d ..\seed_plugins\%%d
 	)
+	for /f "skip=1 tokens=3* " %%c in (folder) do (
+		if not %%d == .. (
+			if not %%d == . (
+					echo %%d
+					xcopy %%d ..\seed_plugins\%%d /i
+				)
+			)
+	)
 	del fileList
+	del folder
 	cd ..
 )
-
-del foldersList
 
 pause>nul
